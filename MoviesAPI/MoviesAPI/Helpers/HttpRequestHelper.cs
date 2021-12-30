75554159fb0477e3b0d17dece5bfcba851baf4f1
modje <djellebm@hotmail.fr>
2021-12-30 14:09:30 +0100

    Add Downloading to httpRequestHelper

--- MoviesAPI/MoviesAPI/Helpers/HttpRequestHelper.cs
+++ MoviesAPI/MoviesAPI/Helpers/HttpRequestHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,11 @@ namespace MoviesAPI.Helpers
     internal static class HttpRequestHelper
     {
         static HttpClient client;
+        const string userAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
         private static void InitHttpClient()
         {
             if (client == null)
             {
-                var userAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";             
                 client = new HttpClient();
                 client.DefaultRequestHeaders.Add("User-Agent", userAgent);
             }
@@ -26,26 +27,42 @@ namespace MoviesAPI.Helpers
         #region Get
         public static async Task<string> GetAsync(Uri url)
         {
-            return await PerformGetCallAsync(url);
+            return await PerformGetStringCallAsync(url);
         }
 
         public static async Task<T> GetAsync<T>(Uri url) where T : class
         {
-            var result = await PerformGetCallAsync(url);
+            var result = await PerformGetStringCallAsync(url);
 
             return JsonHelper.ToObject<T>(result);
         }
 
         public static Task<string> GetAsync(string baseUrl, NameValueCollection parameters)
         {
-            return PerformGetCallAsync(BuildUri(baseUrl, parameters));
+            return PerformGetStringCallAsync(BuildUri(baseUrl, parameters));
         }
 
         public static async Task<T> GetAsync<T>(string baseUrl, NameValueCollection parameters) where T : class
         {
-            var result = await PerformGetCallAsync(BuildUri(baseUrl, parameters));
+            var result = await PerformGetStringCallAsync(BuildUri(baseUrl, parameters));
 
             return JsonHelper.ToObject<T>(result);
+        }
+
+        public static async Task<byte[]> DownloadAsync(Uri url, IEnumerable<KeyValuePair<string, string>> httpRequestHeaders, bool keepHeaders)
+        {
+            foreach (var header in httpRequestHeaders)
+                client.DefaultRequestHeaders.Add(header.Key, header.Value);
+
+            var result = await DownloadFile(url);
+
+            if (!keepHeaders)
+            {
+                foreach (var header in httpRequestHeaders)
+                    client.DefaultRequestHeaders.Remove(header.Key);
+            }
+
+            return result;
         }
 
         private static Uri BuildUri(string url, NameValueCollection parameters)
@@ -63,12 +80,31 @@ namespace MoviesAPI.Helpers
         }
 
 
-        private static async Task<string> PerformGetCallAsync(Uri url)
+        private static async Task<string> PerformGetStringCallAsync(Uri url)
         {
             try
             {
                 InitHttpClient();
                 return await client.GetStringAsync(url);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static async Task<byte[]> DownloadFile(Uri url)
+        {
+            try
+            {
+                InitHttpClient();
+                var response = await client.GetAsync(url);
+
+                var responseStream = response.Content.ReadAsStream();
+                var data = new byte[responseStream.Length];
+
+                responseStream.Read(data, 0, data.Length);
+                return data;
             }
             catch (Exception ex)
             {
