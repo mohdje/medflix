73a5d850f2ddc52225a52588a0326eddef02a059
modje <djellebm@hotmail.fr>
2021-12-31 17:49:36 +0100

    improve test sample

---
+++ MoviesAPI/MoviesApiSample/MoviesAPIServiceFactories.cs
@@ -0,0 +1,18 @@
+﻿using MoviesAPI.Services.Subtitles;
+using MoviesAPI.Services.VFMovies;
+using MoviesAPI.Services.VOMovies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesApiSample
+{
+    internal static class MoviesAPIServiceFactories
+    {
+        public static VOMovieSearcherFactory VOMovieSearcherFactory => new VOMovieSearcherFactory();
+        public static VFMovieSearcherFactory VFMovieSearcherFactory => new VFMovieSearcherFactory();
+        public static SubtitlesProviderFactory SubtitlesProviderFactory => new SubtitlesProviderFactory();
+    }
+}
--- MoviesAPI/MoviesApiSample/Program.cs
+++ MoviesAPI/MoviesApiSample/Program.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using MoviesAPI.Services.VOMovies;
 using MoviesAPI.Services.Subtitles;
 using MoviesAPI.Services.CommonDtos;
+using MoviesAPI.Services.VFMovies;
+using MoviesAPI.Services;
 
 namespace MoviesApiSample
 {
@@ -22,20 +24,56 @@ namespace MoviesApiSample
             //SearchVOMovies("spider man", VOMovieService.YtsApiMx);
             //GetLastMoviesByGenre(VOMovieService.YtsApiMx, "Action");
             //GetSuggestedMovies(VOMovieService.YtsApiMx);
-            
+
+            //SearchVFMovies("inside man", 2006);
+
             //PingService();
-            
-            GetSubtitles(SubtitlesService.OpenSubtitlesHtml);
+
+            //GetSubtitles(SubtitlesService.OpenSubtitlesHtml);
+
+            //GetVOMovieServicesInfo();
+            //GetVFMovieServicesInfo();
+            GetSubtitlesServicesInfo();
 
             Console.WriteLine("End");
             Console.ReadKey();
+        }
+
+        static void GetVOMovieServicesInfo()
+        {
+            Console.WriteLine("VOMovieServices");
+            var serviceInfos = MoviesAPIServiceFactories.VOMovieSearcherFactory.GetServicesInfo();
+            foreach (var info in serviceInfos)
+            {
+                Console.WriteLine($"{info.Description}, {info.Id} : {(info.Available ? "available" : "unaivalable")}");
+            }
+        }
+
+        static void GetVFMovieServicesInfo()
+        {
+            Console.WriteLine("VFMovieServices");
+            var serviceInfos = MoviesAPIServiceFactories.VFMovieSearcherFactory.GetServicesInfo();
+            foreach (var info in serviceInfos)
+            {
+                Console.WriteLine($"{info.Description}, {info.Id} : {(info.Available ? "available" : "unaivalable")}");
+            }
+        }
+
+        static void GetSubtitlesServicesInfo()
+        {
+            Console.WriteLine("SubtitlesServices");
+            var serviceInfos = MoviesAPIServiceFactories.SubtitlesProviderFactory.GetServicesInfo();
+            foreach (var info in serviceInfos)
+            {
+                Console.WriteLine($"{info.Description}, {info.Id} : {(info.Available ? "available" : "unaivalable")}");
+            }
         }
 
         static void SearchVOMovies(string title, VOMovieService movieService)
         {
             Console.WriteLine($"Search VO movies for {title}");
 
-            var moviesServiceProvider = VOMovieSearcherFactory.GetMovieService(movieService);
+            var moviesServiceProvider = MoviesAPIServiceFactories.VOMovieSearcherFactory.GetService(movieService);
             var watch = Stopwatch.StartNew();
             var movies = moviesServiceProvider.GetMoviesByNameAsync(title).Result;
             watch.Stop();
@@ -49,7 +87,7 @@ namespace MoviesApiSample
         {
             Console.WriteLine("Get last movies by genre...");
 
-            var moviesServiceProvider = VOMovieSearcherFactory.GetMovieService(movieService);
+            var moviesServiceProvider = MoviesAPIServiceFactories.VOMovieSearcherFactory.GetService(movieService);
             var watch = Stopwatch.StartNew();
             var movies = moviesServiceProvider.GetLastMoviesByGenreAsync(12, genre).Result;
             watch.Stop();
@@ -61,7 +99,7 @@ namespace MoviesApiSample
         {
             Console.WriteLine("Get suggested movies...");
 
-            var moviesServiceProvider = VOMovieSearcherFactory.GetMovieService(movieService);
+            var moviesServiceProvider = MoviesAPIServiceFactories.VOMovieSearcherFactory.GetService(movieService);
             var watch = Stopwatch.StartNew();
             var movies = moviesServiceProvider.GetSuggestedMoviesAsync(6).Result;
             watch.Stop();
@@ -83,13 +121,13 @@ namespace MoviesApiSample
                 Console.WriteLine("No movies found");
         }
 
-        static void SearchVFMovies(string title)
+        static void SearchVFMovies(string title, int year)
         {
-            //var vfSearcher = new VfTorrentSearcher.MonTorrentMovieSearcher();
+            var vfSearcher = MoviesAPIServiceFactories.VFMovieSearcherFactory.GetService(VFMoviesService.OxTorrent);
 
-            //Console.WriteLine($"Search VF movies for {title}");
+            Console.WriteLine($"Search VF movies for {title}");
 
-            //var movies = vfSearcher.SearchVfAsync(title).Result;
+            var movies = vfSearcher.GetMovieTorrentsAsync(title, year, true).Result;
 
 
             //Console.WriteLine($"VF movies found for {title} :");
@@ -100,17 +138,9 @@ namespace MoviesApiSample
             //Console.WriteLine();
         }
 
-        static void PingService(VOMovieService movieService)
-        {
-            var moviesServiceProvider = VOMovieSearcherFactory.GetMovieService(movieService);
-            var isOk = moviesServiceProvider.PingAsync().Result;
-
-            Console.WriteLine(isOk ? "service OK" : "service HS");
-        }
-
         static void GetSubtitles(SubtitlesService subtitlesService)
         {
-            var subtitlesProvider = SubtitlesProviderFactory.GetSubtitlesProvider(subtitlesService);
+            var subtitlesProvider = MoviesAPIServiceFactories.SubtitlesProviderFactory.GetService(subtitlesService);
             var result = subtitlesProvider.GetAvailableSubtitlesAsync("tt0816692", SubtitlesLanguage.French).Result;
 
             if (result == null)
