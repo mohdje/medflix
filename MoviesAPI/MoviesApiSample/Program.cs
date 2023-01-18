96433ed65bcca8d2e48ba2a00614716a5fc6bfb8
modje <djellebm@hotmail.fr>
2023-01-18 09:20:45 +0100

    update movies API sample

--- MoviesAPI/MoviesApiSample/Program.cs
+++ MoviesAPI/MoviesApiSample/Program.cs
@@ -12,174 +12,23 @@ using MoviesAPI.Services.Subtitles;
 using MoviesAPI.Services;
 using System.Threading.Tasks;
 using MoviesAPI.Helpers;
-using MoviesAPI.Services.Movies.Dtos;
+using MoviesAPI.Services.Content.Dtos;
 
 namespace MoviesApiSample
 {
     class Program
     {
-        const string apiKey = Tokens.API_TOKEN;
-        static void Main(string[] args)
+       
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Test started");
+            var movies = new Movies(Tokens.API_TOKEN);
+            var series = new Series(Tokens.API_TOKEN);
 
-            //SearchMovies("rocky");
-            // GetMoviesOfToday();
-           // GetPopularMovies();
-            //GetPopularMoviesByGenre(16);
-            //GetMoviesByGenre(10751);
-            //GetTopNetflixMovies();
-            // GetMovieDetails("436270");
-            //GetPopularNetflixMovies();
-            //GetPopularDisneyPlusMovies();
-            //GetPopularAmazonPrimeMovies();
-
-
-            //GetFrenchTitle("718930");
-            //GetGenres();
-
-            //SearchVfTorrents("american girl", 2022);
-            //SearchVoTorrents("Project Gemini", 2022);
-            GetSubtitles("tt1441105", SubtitlesLanguage.French);//tt16194408 "tt1441105"
+            await movies.Test();
+            //await series.Test();
 
             Console.ReadKey();
-        }
-
-        static async Task GetMoviesOfToday()
-        {
-            Console.WriteLine("Movies of the day");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetMoviesOfTodayAsync());
-        }
-
-        static async Task GetPopularMovies()
-        {
-            Console.WriteLine("Popular movies");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetPopularMoviesAsync());
-        }
-
-        static async Task GetPopularNetflixMovies()
-        {
-            Console.WriteLine("Popular Netflix movies");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetPopularNetflixMoviesAsync());
-        }
-
-        static async Task GetPopularDisneyPlusMovies()
-        {
-            Console.WriteLine("Popular DisneyPlus movies");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetPopularDisneyPlusMoviesAsync());
-        }
-
-        static async Task GetPopularAmazonPrimeMovies()
-        {
-            Console.WriteLine("Popular Amazon prime movies");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetPopularAmazonPrimeMoviesAsync());
-        }
-
-        static async Task GetPopularMoviesByGenre(int genreId)
-        {
-            Console.WriteLine($"Popular movies by genre : {genreId}");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetPopularMoviesByGenreAsync(genreId));
-        }
-
-        static async Task GetMoviesByGenre(int genreId)
-        {
-            Console.WriteLine($"Movies by genre : {genreId}");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetMoviesByGenreAsync(genreId, 1));
-        }
-
-        static async Task SearchMovies(string movieName)
-        {
-            Console.WriteLine($"Search movie: {movieName}");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.SearchMoviesAsync(movieName));
-        }
-
-        static async Task GetGenres()
-        {
-            Console.WriteLine("Genres");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            var genres = await movieSearcher.GetGenresAsync();
-
-            foreach (var genre in genres)
-            {
-                Console.WriteLine(genre.Name);
-            }
-        }
-
-        static async Task GetFrenchTitle(string movieId)
-        {
-            Console.WriteLine($"GetFrenchTitle {movieId}");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            var title = await movieSearcher.GetFrenchTitleAsync(movieId);
-
-            Console.WriteLine(title);
-        }
-
-        static async Task GetTopNetflixMovies()
-        {
-            Console.WriteLine("Top Netflix movies");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-            ShowMoviesList(await movieSearcher.GetTopNetflixMoviesAsync());
-        }
-
-        static async Task GetMovieDetails(string movieId)
-        {
-            Console.WriteLine("Movie Details");
-
-            var movieSearcher = MoviesAPIFactory.Instance.CreateMovieSearcher(apiKey);
-
-
-            var movie = await movieSearcher.GetMovieDetailsAsync(movieId);
-
-            Console.WriteLine($"{movie.Id}. {movie.Title}  ({movie.Year}), {movie.Rating}, background:{movie.BackgroundImageUrl}, cover:{movie.CoverImageUrl}, synopsis: {movie.Synopsis}");
-            Console.WriteLine($"Genre: {movie.Genre}");
-            Console.WriteLine($"Director: {movie.Director}");
-            Console.WriteLine($"Cast: {movie.Cast}");
-            Console.WriteLine($"Duration: {movie.Duration}");
-            Console.WriteLine($"Trailer: {movie.YoutubeTrailerUrl}");
-
-        }
-
-        static void ShowMoviesList(IEnumerable<LiteMovieDto> movies)
-        {
-            if (movies == null || !movies.Any())
-                Console.WriteLine("No result");
-            else
-            {
-                foreach (var movie in movies)
-                {
-                    Console.WriteLine($"{movie.Id}. {movie.Title}  ({movie.Year}), {movie.Rating}, background:{movie.BackgroundImageUrl}, cover:{movie.CoverImageUrl}, logo:{movie.LogoImageUrl}, synopsis: {movie.Synopsis}");
-                }
-            }
         }
 
         static async Task SearchVfTorrents(string frenchTitleMovie, int year)
---
+++ MoviesAPI/MoviesApiSample/Series.cs
@@ -0,0 +1,148 @@
+﻿using MoviesAPI.Services;
+using MoviesAPI.Services.Content;
+using MoviesAPI.Services.Content.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesApiSample
+{
+    internal class Series
+    {
+        ISeriesSearcher seriesSearcher;
+        public Series(string apiKey)
+        {
+            seriesSearcher = MoviesAPIFactory.Instance.CreateSeriesSearcher(apiKey);
+        }
+
+        public async Task Test()
+        {
+            //await SearchSeries("dexter");
+            //await GetSeriesOfToday();
+
+            //await GetPopularSeries();
+            //await GetPopularSeriesByGenre(16);
+            //await GetSeriesByGenre(10767);
+            // await GetSerieDetails("119051");
+            //await GetPopularNetflixSeries();
+            //await GetPopularDisneyPlusSeries();
+            //await GetPopularAmazonPrimeSeries();
+
+            await GetFrenchTitle("119051");
+            await GetGenres();
+
+            //GetSerieEpisodesBySeason("1405", 2);
+        }
+        private async Task GetSeriesOfToday()
+        {
+            ShowSeriesList(await seriesSearcher.GetSeriesOfTodayAsync(), "Series of the day");
+        }
+        private async Task GetPopularSeries()
+        {
+            ShowSeriesList(await seriesSearcher.GetPopularSeriesAsync(), "Popular Series");
+        }
+
+        private async Task GetPopularNetflixSeries()
+        {
+            ShowSeriesList(await seriesSearcher.GetPopularNetflixSeriesAsync(), "Popular Netflix series");
+        }
+
+        private async Task GetPopularDisneyPlusSeries()
+        {
+            ShowSeriesList(await seriesSearcher.GetPopularDisneyPlusSeriesAsync(), "Popular DisneyPlus series");
+        }
+
+        private async Task GetPopularAmazonPrimeSeries()
+        {
+            ShowSeriesList(await seriesSearcher.GetPopularAmazonPrimeSeriesAsync(), "Popular Amazon prime series");
+        }
+
+        private async Task GetPopularSeriesByGenre(int genreId)
+        {
+            ShowSeriesList(await seriesSearcher.GetPopularSeriesByGenreAsync(genreId), $"Popular series by genre : {genreId}");
+        }
+
+        private async Task GetSeriesByGenre(int genreId)
+        {
+            ShowSeriesList(await seriesSearcher.GetSeriesByGenreAsync(genreId, 1), $"Series by genre : {genreId}");
+        }
+
+        private async Task SearchSeries(string movieName)
+        {
+            ShowSeriesList(await seriesSearcher.SearchSeriesAsync(movieName), $"Search serie: {movieName}");
+        }
+
+        private async Task GetGenres()
+        {
+            Console.WriteLine("Genres");
+
+            var genres = await seriesSearcher.GetSerieGenresAsync();
+
+            foreach (var genre in genres)
+            {
+                Console.WriteLine(genre.Name);
+            }
+        }
+
+        private async Task GetFrenchTitle(string serieId)
+        {
+            Console.WriteLine($"GetFrenchTitle {serieId}");
+
+            var title = await seriesSearcher.GetSerieFrenchTitleAsync(serieId);
+
+            Console.WriteLine(title);
+        }
+
+        private async Task GetSerieDetails(string serieId)
+        {
+            Console.WriteLine("Serie Details");
+
+            var serie = await seriesSearcher.GetSerieDetailsAsync(serieId);
+
+            Console.WriteLine($"{serie.Id}. {serie.Title}  ({serie.Year}), {serie.Rating}, background:{serie.BackgroundImageUrl}, cover:{serie.CoverImageUrl}, synopsis: {serie.Synopsis}");
+            Console.WriteLine($"Genre: {serie.Genre}");
+            Console.WriteLine($"Director: {serie.Director}");
+            Console.WriteLine($"Cast: {serie.Cast}");
+            Console.WriteLine($"Trailer: {serie.YoutubeTrailerUrl}");
+            Console.WriteLine($"Seasons: {serie.SeasonsCount}");
+
+        }
+
+        private async Task GetSerieEpisodesBySeason(string serieId, int seasonNumber)
+        {
+            Console.WriteLine("Serie Episodes");
+
+            var episodes = await seriesSearcher.GetEpisodes(serieId, seasonNumber);
+
+            foreach (var episode in episodes)
+            {
+                Console.WriteLine($"Name: {episode.Name}");
+                Console.WriteLine($"EpisodeNumber: {episode.EpisodeNumber}");
+                Console.WriteLine($"RunTime: {episode.RunTime}");
+                Console.WriteLine($"Overview: {episode.Overview}");
+                Console.WriteLine($"ImagePath: {episode.ImagePath}");
+                Console.WriteLine("#############");
+
+            }
+
+
+        }
+
+        private void ShowSeriesList(IEnumerable<LiteContentDto> series, string operationInfo)
+        {
+            Console.WriteLine(operationInfo);
+
+            if (series == null || !series.Any())
+                Console.WriteLine("No result");
+            else
+            {
+                foreach (var serie in series)
+                {
+                    Console.WriteLine($"{serie.Id}. {serie.Title}  ({serie.Year}), {serie.Rating}, background:{serie.BackgroundImageUrl}, cover:{serie.CoverImageUrl}, logo:{serie.LogoImageUrl}, synopsis: {serie.Synopsis}");
+                }
+            }
+        }
+    }
+}
