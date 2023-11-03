03f2a207704c88fce28c5604581b8adbfa7db17b
modje <djellebm@hotmail.fr>
2023-11-03 17:12:36 +0100

    apply correct url for app view

--- Front/desktop-avalonia-ui-app/Medflix/Tools/Consts.cs
+++ Front/desktop-avalonia-ui-app/Medflix/Tools/Consts.cs
@@ -12,6 +12,6 @@ namespace Medflix.Tools
     {
         public const string AppVersionName = "Medflix 2.2.0";
         public const string LatestReleaseUrl = "https://api.github.com/repos/mohdje/medflix/releases/latest";
-        public const string MainAppViewUrl = "http://localhost:3000/index.html";// "http://localhost:5000/home/index.html";
+        public const string MainAppViewUrl = "http://localhost:5000/home/index.html"; //"http://localhost:3000/index.html";// "http://localhost:5000/home/index.html";
     }
 }
