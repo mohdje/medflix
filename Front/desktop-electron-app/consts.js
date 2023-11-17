5c79391cfdb9c9da954fd4c347cb99d4235b229e
modje <djellebm@hotmail.fr>
2023-11-17 11:30:52 +0100

    remove useless projects

--- Front/desktop-electron-app/consts.js
+++
@@ -1,4 +0,0 @@
-const backendUrl = 'http://localhost:5000/application/';
-const icon = process.platform === 'darwin' ? 'Medflix.icns' : 'views/app/favicon.ico';
-
-module.exports = { backendUrl, icon };
--- Front/desktop-electron-app/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  confirmUpdateApp: () => ipcRenderer.send('confirm-update-app'),
-  cancelUpdateApp: () => ipcRenderer.send('cancel-update-app'),
-  onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', callback),
-})
