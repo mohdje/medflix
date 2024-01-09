f4f1bb151d78bdbe520509fee6f4a91088a11efb
modje <djellebm@hotmail.fr>
2024-01-09 16:19:34 +0100

    bump version

--- Front/desktop-avalonia-ui-app/Medflix/Tools/Consts.cs
+++ Front/desktop-avalonia-ui-app/Medflix/Tools/Consts.cs
@@ -4,7 +4,7 @@ namespace Medflix.Tools
     public static class Consts
     {
         public static string AppVersionName => $"Medflix {AppVersion}";
-        public const string AppVersion = "2.2.0";
+        public const string AppVersion = "2.2.1";
         public const string LatestReleaseUrl = "https://api.github.com/repos/mohdje/medflix/releases/latest";
 
         //use react project for debug http://localhost:3000/index.html
