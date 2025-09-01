a7a6ce0068f7162188396da8f1f296edb1a309aa
modje <djellebm@hotmail.fr>
2025-09-01 11:42:51 +0100

    delete unsused api

--- Client/ReactApp/src/services/api/index.js
+++ Client/ReactApp/src/services/api/index.js
@@ -2,10 +2,8 @@ import * as mediasInfoApi from './mediasInfoApi';
 import * as bookmarkApi from './bookmarkApi';
 import * as mediaSourcesApi from './mediaSourcesApi';
 import * as watchHistoryApi from './watchHistoryApi';
-import * as torrentApi from './torrentApi';
 
 export { mediasInfoApi };
 export { bookmarkApi };
 export { mediaSourcesApi };
 export { watchHistoryApi };
-export { torrentApi };
