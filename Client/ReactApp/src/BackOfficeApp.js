b644cd5aa254118542aa22b063abc95ff0429466
mohdje <djellebm@hotmail.fr>
2026-09-15 10:34:20 +0100

    add menu for backoffice

--- Client/ReactApp/src/BackOfficeApp.js
+++ Client/ReactApp/src/BackOfficeApp.js
@@ -1,11 +1,42 @@
 import AvailableMediasVideosPage from "./pages/AvailableMediasVideosPage";
+import "./style/css/back-office-app.css";
+import { useState } from "react";
 
 function BackOfficeApp() {
-  return (
-    <>
-      <AvailableMediasVideosPage />
-    </>
-  );
+  const [selectedMenuItemId, setSelectedMenuItemId] = useState(1);
+
+  const handleMenuItemClick = (menuItem) => {
+    setSelectedMenuItemId(menuItem.id);
+  };
+
+  return <div className="back-office-app">
+    <LeftMenu
+      selectedMenuItemId={selectedMenuItemId}
+      onMenuItemClick={handleMenuItemClick}
+    />
+    <main className="back-office-content">
+      {selectedMenuItemId === 1 && <AvailableMediasVideosPage />}
+    </main>
+  </div>
+}
+
+function LeftMenu({ selectedMenuItemId, onMenuItemClick }) {
+  const menuItems = [
+    { id: 1, label: "Available Medias Videos" },
+    { id: 2, label: "Errors" }
+  ];
+
+  return <nav className="back-office-menu">
+    {menuItems.map((menuItem) => (
+      <h3
+        key={menuItem.id}
+        className={menuItem.id === selectedMenuItemId ? "selected" : ""}
+        onClick={() => onMenuItemClick(menuItem)}
+      >
+        {menuItem.label}
+      </h3>
+    ))}
+  </nav>
 }
 
 export default BackOfficeApp;
