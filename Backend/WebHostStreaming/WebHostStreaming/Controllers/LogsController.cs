5f4b58ca1fedd037512ebf05e14eece745989121
mohdje <djellebm@hotmail.fr>
2026-09-18 10:57:48 +0100

    implement back SSE for live logs

--- Backend/WebHostStreaming/WebHostStreaming/Controllers/LogsController.cs
+++ Backend/WebHostStreaming/WebHostStreaming/Controllers/LogsController.cs
@@ -1,31 +1,41 @@
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using WebHostStreaming.Helpers;
 using WebHostStreaming.Providers;
 
 namespace WebHostStreaming.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class ErrorLogsController : ControllerBase
+    public class LogsController : ControllerBase
     {
         private readonly IErrorLogsProvider _errorLogsProvider;
 
-        public ErrorLogsController(IErrorLogsProvider errorLogsProvider)
+        public LogsController(IErrorLogsProvider errorLogsProvider)
         {
             _errorLogsProvider = errorLogsProvider;
         }
 
-        [HttpGet]
+        [HttpGet("errors")]
         public IActionResult GetErrorLogs()
         {
             return Ok(_errorLogsProvider.GetErrorLogs());
         }
 
-        [HttpDelete]
+        [HttpDelete("errors")]
         public IActionResult DeleteErrorLogs()
         {
             _errorLogsProvider.DeleteErrorLogs();
 
             return NoContent();
+        }
+
+        [HttpGet("live")]
+        public ServerSentEventsResult<string> GetLiveLogs(CancellationToken cancellationToken)
+        {
+            return TypedResults.ServerSentEvents(AppLogger.GetLogs(cancellationToken), eventType: "live_logs");
         }
     }
 }
--- Backend/WebHostStreaming/WebHostStreaming/Helpers/AppLogger.cs
+++ Backend/WebHostStreaming/WebHostStreaming/Helpers/AppLogger.cs
@@ -1,36 +1,78 @@
-﻿using System.Text;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
 
 namespace WebHostStreaming.Helpers
 {
     public static class AppLogger
     {
+        private const int MaxLogCount = 100;
+        private static readonly Channel<string> Logs = Channel.CreateBounded<string>(
+            new BoundedChannelOptions(MaxLogCount)
+            {
+                FullMode = BoundedChannelFullMode.DropOldest,
+                SingleReader = false,
+                SingleWriter = false
+            });
+
         public static void LogInfo(string message)
         {
             var logmessage = $"-{GetDateTime()} : {message}";
 
-            Console.WriteLine(logmessage);
+            WriteLog(logmessage);
         }
 
         public static void LogInfo(string clientAppId, string message)
         {
             var logmessage = $"-{GetDateTime()}, Client-App-Id {clientAppId} : {message}";
 
-            Console.WriteLine(logmessage);
+            WriteLog(logmessage);
         }
 
         public static void LogError(string functionName, Exception exception)
         {
             var logmessage = $"-{GetDateTime()} : Error in {functionName}, {exception.GetBaseException().Message}";
 
-            Console.WriteLine(logmessage);
+            WriteLog(logmessage);
         }
 
         public static void LogError(string clientAppId, string functionName, Exception exception)
         {
             var logmessage = $"-{GetDateTime()}, Client-App-Id {clientAppId} : Error in {functionName}, {exception.GetBaseException().Message}";
 
+            WriteLog(logmessage);
+        }
+
+        private static void WriteLog(string logmessage)
+        {
+            Logs.Writer.TryWrite(logmessage);
+
             Console.WriteLine(logmessage);
+        }
+
+        public static async IAsyncEnumerable<string> GetLogs(
+                [System.Runtime.CompilerServices.EnumeratorCancellation] CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                bool hasLogs;
+
+                try
+                {
+                    hasLogs = await Logs.Reader.WaitToReadAsync(ct);
+                }
+                catch (OperationCanceledException) when (ct.IsCancellationRequested)
+                {
+                    yield break;
+                }
+
+                if (!hasLogs)
+                    yield break;
+
+                while (Logs.Reader.TryRead(out var log))
+                    yield return log;
+            }
         }
 
         private static string GetDateTime()
